b256adc8c8de69adbe71d7a8564807ffdbe344e0
Charlotte Bourg <127722522+charlotte-bourg@users.noreply.github.com>
2023-06-13 14:11:35 -0700

    Time: 59 ms (10.91%), Space: 16.6 MB (20.71%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,10 +1,9 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # for i, char in enumerate(str[0]):
-        #     for str[0]
+        # single element means full string is common prefix 
         if len(strs) == 1:
             return strs[0]
-        # if there is more than one element in the list
+        # otherwise sort 
         strs.sort() # O(n)
         first = strs[0]
         last = strs[len(strs) - 1]
@@ -15,5 +14,3 @@ class Solution:
             else:
                 return prefix 
         return prefix 
-        # check if whole string at strs[0] matches 
-
