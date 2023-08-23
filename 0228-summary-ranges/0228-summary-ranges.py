787e650578ca46315295a7e0cdc3f25ae21bbe53
Charlotte Bourg <127722522+charlotte-bourg@users.noreply.github.com>
2023-08-23 11:55:24 -0700

    Time: 45 ms (40.98%), Space: 16.4 MB (37.21%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -21,34 +21,3 @@ class Solution:
         else:
             a.append(f'{nums[s]}')
         return a
-
-# [0,1,2,4,5,7]
-# s = 0
-# e = 0 
-# i = 0 
-# does nums[1] == nums[0] + 1? yes
-#   e = 1
-# i = 1
-# does nums[2] == nums[1] + 1? yes
-#   e = 2
-# i = 2
-# does nums[3] == nums[2] + 1? no
-#   does s = e? no
-#   append 0 -> 2
-#   s = 3
-#   e = 3 
-# i = 3
-# does nums[4] = nums[3] + 1? yes
-#   e = 4
-# i = 4 
-# does nums[5] = nums[4] + 1? no
-#   does s= e? no
-#       append 4->5
-#
-
-
-# pseudocode
-# given examples
-# [0,1,2,4,5,7]
-# whenever nums[i+1] is nums[i] + 1, include in the current range
-# othersise mark as its own number 
