4fd2415ef99f7e97457e6086da06db62e120fff6
Charlotte Bourg <127722522+charlotte-bourg@users.noreply.github.com>
2023-09-21 13:12:26 -0700

    Time: 38 ms (60.03%), Space: 16.3 MB (41.06%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,47 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        r = 0
+        rMin = 1
+        rMax = len(matrix) - 1 
+        c = 0 
+        cMin = 0
+        cMax = len(matrix[0]) - 1 
+        if cMax == 0:
+            dir = 1
+        else:
+            dir = 0 
+        ret = [matrix[r][c]]
+        for _ in range(len(matrix) * len(matrix[0]) - 1):
+            # right
+            if dir == 0: 
+                c += 1
+                if c == cMax: 
+                    dir += 1 
+                    cMax -= 1 
+            # down
+            elif dir == 1:
+                r += 1 
+                if r == rMax:
+                    dir += 1 
+                    rMax -= 1 
+            # left
+            elif dir == 2:
+                c -= 1
+                if c == cMin:
+                    dir += 1 
+                    cMin += 1 
+            # up
+            else: 
+                r -= 1
+                if r == rMin:
+                    dir = 0 
+                    rMin += 1 
+            ret.append(matrix[r][c])
+            print(f"matrix[{r}][{c}]")
+        return ret 
+
+# [1 ,2 ,3 ,4 ,5 ],
+# [6 ,7 ,8 ,9 ,10],
+# [11,12,13,14,15],
+# [16,17,18,19,20],
+# [21,22,23,24,25]]
