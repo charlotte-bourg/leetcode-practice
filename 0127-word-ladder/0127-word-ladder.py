5207848aeca9d803475ae1261e8cfc8bf80df556
Charlotte Bourg <127722522+charlotte-bourg@users.noreply.github.com>
2023-11-08 18:11:02 -0800

    Time: 454 ms (27.48%), Space: 17.8 MB (79.44%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -5,6 +5,7 @@ class Solution:
         # shortest path -> BFS
         wordList = set(wordList) # O(n) where n is len wordList
 
+        l = len(beginWord) # words are the same length 
         ALPHA = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         words = deque([(beginWord, 1)])
         seenWords = {beginWord}
@@ -15,12 +16,11 @@ class Solution:
             if currWord == endWord:
                 return steps
 
-            for char in ALPHA:
-                for i in range(len(currWord)):
+            for char in ALPHA: # O(1)
+                for i in range(l): # O(m) where m is len words
                     neighbor = currWord[:i] + char + currWord[i+1:]
                     if neighbor not in seenWords and neighbor in wordList:
                         words.append([neighbor, steps + 1])
-                        print(f"neighbor added is: {neighbor} in {steps+1}")
                         seenWords.add(neighbor)
             
         return 0 
